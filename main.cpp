6bb17d2d29110f367a3871328319f59296403586
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-02-16 17:06:12 +0900

    必要なﾘｿｰｽをコピーしmain.cppを定義

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <Adapter.h>
+//#include <scene/user/SampleTitle.h>
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//LWP::System::Run(new SampleTitle);
+	return 0;
+}
