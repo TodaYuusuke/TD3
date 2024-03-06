bb9fc563a2fecebc5e2dc8b1d5cab4cb962490fe
TodaYuusuke <115054969+TodaYuusuke@users.noreply.github.com>
2024-03-06 11:08:17 +0900

    バッチファイルを更新

--- LWP_Update.bat
+++ LWP_Update.bat
@@ -1,4 +1,5 @@
 @echo off
+
 REM Visual Studio Developer Command Promptを使用する場合
 REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
@@ -8,13 +9,17 @@ REM 変更をすべて取り消す
 git reset --hard
 REM Gitで最新のコードを取得
 git pull origin LWP_20231213
+if ERRORLEVEL 1 (
+    REM ここにエラー発生時の処理を記述します
+    echo.
+    echo git command could not be executed.
+    echo Please check if git is installed.
+    echo Please ask your administrator for error details.
+    echo.
+    pause
+    exit /b
+)
 
-REM MSBuildのパス
-REM set MSBUILD_PATH="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-REM VSINSTALLDIR=MSBuild\Microsoft\VC\v170\
-
-REM Visual Studioでビルド
-REM %MSBUILD_PATH% "DirectXGame.sln"
 
 REM Debugでビルド
 MSBuild "DirectXGame.sln" /t:clean;rebuild /p:ConfigurationType=StaticLibrary;Configuration=Debug;MultiProcessorCompilation=true
