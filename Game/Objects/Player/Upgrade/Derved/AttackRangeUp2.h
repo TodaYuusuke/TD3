c09f794086db7655a8d18f0ad870a157da80c67d
kura495 <104417866+kura495@users.noreply.github.com>
2024-03-27 16:22:11 +0900

    dekita

---
+++ Game/Objects/Player/Upgrade/Derved/AttackRangeUp2.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../IUpgrade.h"
+
+namespace L
+{
+	/// <summary>
+	/// 純粋な攻撃範囲アップ！（定数）
+	/// </summary>
+	class AttackRangeUp2 : public IUpgrade
+	{
+
+		/// <summary>
+		/// アップグレードの内容
+		/// </summary>
+		void Apply(UpgradeParameter* para) override
+		{
+			para->attackRange.base += 2.0f;
+		}
+
+		std::string GetTexturePass() override
+		{
+			return "powerUp/PowerUp.png";
+		}
+	};
+}
