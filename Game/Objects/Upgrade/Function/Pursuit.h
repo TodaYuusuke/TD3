6c12505ecff3b04d8b1f01d1e688739911343822
kura495 <k024@6001@g.neec.ac.jp>
2024-04-19 13:04:05 +0900

    追撃のダメージアップ

--- Game/Objects/Upgrade/Function/Pursuit.h
+++ Game/Objects/Upgrade/Function/Pursuit.h
@@ -14,7 +14,7 @@ private:
 	// ボスも含めたすべての敵
 	std::vector<IEnemy*> enemys_;
 	// 攻撃力
-	int damage_ = 1;
+	int damage_ = 10;
 	const int kInterval = 120;
 	int interval = 120;
 	std::vector<LWP::Primitive::Mesh> models_;
