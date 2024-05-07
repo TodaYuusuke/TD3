26bb8c17c26eac9caa7d989974f917e25c6498bc
pepe <kousaku5963@gmail.com>
2024-05-07 15:06:16 +0900

    同じシーンに飛ぶとバグるど

--- Game/Objects/Player/Weapon/Weapon.cpp
+++ Game/Objects/Player/Weapon/Weapon.cpp
@@ -57,7 +57,7 @@ void Weapon::Update()
 void Weapon::InitRoot()
 {
 	rootData_.translate_.start_ = demoModel_.transform.translation;
-	rootData_.rotate_.start_ = demoModel_.transform.rotation;
+	//rootData_.rotate_.start_ = demoModel_.transform.rotation;
 }
 
 void Weapon::InitSlash()
@@ -67,7 +67,7 @@ void Weapon::InitSlash()
 void Weapon::InitMoment()
 {
 	momentData_.scale_.start_ = demoModel_.transform.scale;
-	momentData_.rotate_.start_ = demoModel_.transform.rotation;
+	//momentData_.rotate_.start_ = demoModel_.transform.rotation;
 	momentData_.translate_.start_ = demoModel_.transform.translation;
 }
 
