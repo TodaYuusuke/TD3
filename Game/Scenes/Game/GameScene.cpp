e8340b2dc13ec1f23edefe05bd658159d2447297
k024g6001@g.neec.ac.jp <k024g6001@g.neec.ac.jp>
2024-05-21 01:42:13 +0900

    デバッグ表示消した

--- Game/Scenes/Game/GameScene.cpp
+++ Game/Scenes/Game/GameScene.cpp
@@ -83,8 +83,7 @@ void GameScene::Initialize()
 	sun_.radius = 105.0f;
 	sun_.decay = 0.58f;
 
-	// フレームレートを表示する関数（後で消すこと！）
-	Info::ChangeShowDebugGUI();
+
 
 
 	sceneTransition_ = std::make_unique<SceneTransition>();
