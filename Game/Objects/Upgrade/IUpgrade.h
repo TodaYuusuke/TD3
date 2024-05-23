56dfd88086e3a2d8747c51d2d645585ef2cbde86
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-05-23 10:44:23 +0900

    IUpgrade.hのコメントを削除

--- Game/Objects/Upgrade/IUpgrade.h
+++ Game/Objects/Upgrade/IUpgrade.h
@@ -96,51 +96,18 @@ namespace L
 	{
 	public:	// ** パブリックなメンバ変数 ** //
 
-		// 適応ずみかフラグ
-		//bool isApplied = false;
-
-
 		/// <summary>
 		/// アップグレードを適応する関数
-		/// <para>最後にisAppliedを変更すること！</para>
 		/// </summary>
 		/// <param name="p"></param>
 		virtual void Apply(UpgradeParameter* para) = 0;
 
 		/// <summary>
-		/// UI用のテクスチャのパスを返す関数
-		/// </summary>
-		/// <returns>resources/texture/以降のパス</returns>
-		//virtual std::string GetTexturePass() = 0;
-
-		/// <summary>
 		/// デバッグ表示関数
 		/// </summary>
-		virtual std::string GetUpgradeName() = 0;
-
-		/// <summary>
-		/// 初期化（sprite用テクスチャ読み込み）
-		/// </summary>
-		//void Init();
-
-		/// <summary>
-		/// 更新（アニメーションとかあるかもなので用意）
-		/// <para>必ずSpriteのisActiveをfalseに！</para>
-		/// <para>特殊なアニメーションとかしたいなら継承</para>
-		/// </summary>
-		//virtual void Update();
-
-		/// <summary>
-		/// スプライト表示
-		/// <para>この関数を呼び出さないとそのフレームは表示されない（SpriteのisActiveをtrueにしてね）</para>
-		/// </summary>
-		/// <param name="pos"></param>
-		//void ShowUI(const LWP::Math::Vector2& pos);
-
+		virtual std::string GetUpgradeName() { return "Upgrade"; }
 
 	protected: // ** 派生先用のメンバ変数 ** //
 
-		// スプライト
-		//LWP::Primitive::Sprite sprite_;
 	};
 }
